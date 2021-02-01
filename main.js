541ae27336ec83d4ce2e4c68c89d655999d321c6
Adalberto R. Teixeira <adalbertoart@gmail.com>
2021-02-01 13:48:51 -0300

    Definido movimentos da cobra

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ snake[0] = {
   x: 8 * box,
   y: 8 * box,
 };
+let direction = "rigth";
 
 function criarBG() {
   context.fillStyle = "lightgreen";
@@ -19,5 +20,36 @@ function criaCobrinha() {
   }
 }
 
-criarBG();
-criaCobrinha();
+function iniciarJogo() {
+  criarBG();
+  criaCobrinha();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  switch (direction) {
+    case "rigth":
+      snakeX += box;
+      break;
+    case "left":
+      snakeX -= box;
+      break;
+    case "up":
+      snakeY -= box;
+      break;
+    case "down":
+      snakeY += box;
+      break;
+  }
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+
+  snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
